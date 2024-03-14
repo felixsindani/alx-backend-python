cb63dbff0f7b326280ea5e76dea7bc64b624c27d
felixsindani <felixsindani72@gmail.com>
2024-03-14 20:58:06 +0300

    Tasks

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+'''
+type-annotated function floor which takes a float n as argument and returns the floor of the float.
+'''
+
+
+def floor(n: float) -> int:
+    ''' Return largest int value less than or equal to n. '''
+    return int(n) if n >= 0 else int(n) - 1
